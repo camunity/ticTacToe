0ea410754dbf145ffdf15af3344b55470a0f00fd
Cameron Flowers <cameron.flowers13@gmail.com>
2020-02-20 18:00:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 /*----- constants -----*/
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
 /*----- app's state (variables) -----*/
 
 let board;
 let turn = 'X';
+let win;
 
 /*----- cached element references -----*/
 
@@ -12,21 +22,29 @@ const squares = Array.from(document.querySelectorAll('#board div'));
 
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
+document.getElementById('reset-button').addEventListener('click', init);
 
 
 /*----- functions -----*/
 
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+        });
+        return winner ? winner : board.includes('') ? null : 'T';
+};
 
-
-function handleTurn(event) {
+function handleTurn() {
     let idx = squares.findIndex(function(square) {
-    return square === event.target;
+        return square === event.target;
     });
-    // new code below
     board[idx] = turn;
-    // check your console logs to make sure it's working!
-    console.log(board);
-    };
+    turn = turn === 'X' ? 'O' : 'X';
+    win = getWinner();
+    render();
+};
 
 function init() {
     board = [
@@ -42,6 +60,7 @@ function render() {
     //this moves the value of the board item into the squares[idx]
     squares[index].textContent = mark;
     });
+    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
     };
 
 init();
