71d699db3b0936549e15b417126e75a9cfdefefd
Cameron Flowers <cameron.flowers13@gmail.com>
2020-07-22 10:15:53 -0500

    finished voting edition

--- js/main.js
+++ js/main.js
@@ -10,9 +10,15 @@ const winningCombos = [
     [2, 4, 6]
     ];
 
+const trumpFaces = ['https://pngimg.com/uploads/donald_trump/donald_trump_PNG77.png','https://img.pngio.com/donald-trump-head-png-donald-trump-head-png-427_547.png','https://clipart.info/images/ccovers/1523212416donald-trump-head-wtf-png.png'];
+
+const bidenFaces = ['https://politico.com/interactives/uploads/2020-elections/headshots/png/300/joe-biden.png'];
+
+const kanyeFaces = ['https://www.pngkey.com/png/full/14-146074_share-this-article-kanye-west-head-png.png']
 /*----- app's state (variables) -----*/
 
 let board;
+let gameOver = false;
 let turn = 'X';
 let win;
 
@@ -33,17 +39,20 @@ function getWinner() {
     winningCombos.forEach(function(combo, index) {
         if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
         });
+        if(winner){gameOver = true}
         return winner ? winner : board.includes('') ? null : 'T';
 };
 
 function handleTurn() {
-    let idx = squares.findIndex(function(square) {
-        return square === event.target;
-    });
-    board[idx] = turn;
-    turn = turn === 'X' ? 'O' : 'X';
-    win = getWinner();
-    render();
+        let idx = squares.findIndex(function(square) {
+            return square === event.target;
+        });
+        if(gameOver == false){
+            board[idx] = turn;
+            turn = turn === 'X' ? 'O' : 'X';
+            win = getWinner();
+            render();
+        }
 };
 
 function init() {
@@ -52,15 +61,28 @@ function init() {
     '', '', '',
     '', '', ''
     ];
+    gameOver = false
+    win = null
+    messages.textContent = `It's ${turn}'s turn!`
     render();
 };
 
 function render() {
     board.forEach(function(mark, index) {
     //this moves the value of the board item into the squares[idx]
-    squares[index].textContent = mark;
+    // squares[index].textContent = mark;
+    if (mark !== "") {
+        let imageURL = mark == "X" ? bidenFaces[0] : trumpFaces[0]
+        squares[index].style.backgroundImage = `url(${imageURL})`
+        squares[index].style.backgroundSize = "100%, 100%"
+        squares[index].style.backgroundRepeat = "no-repeat"
+    } else{
+        squares[index].style.backgroundImage = ``
+    }
     });
-    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    // messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? `It's honestly a tie, but keep clicking to change the result!` : win ? `Capitalism wins! 🇺🇸` : `It's ${turn}'s turn!`;
+
     };
 
 init();
